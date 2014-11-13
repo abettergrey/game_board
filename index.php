a5b5bf396bd8087a19bea39b67335b8d5a899764
Brett Yeager <abettergrey@gmail.com>
2014-11-12 20:40:09 -0500

    Made header REM statment

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-
-echo "Hi Mom!";
+session_start();
 //GAME BOARD
-//
+//Author: Brett Yeager
+//Desc.: This application will allow a user to log in/
+//	create an acount. This account can then join/leave/create a team.
+//	The teams will then create events: multiplayer matches.
+//	Once the match is complete, a team will enter stats about the
+//	match and the other team will have to accept. If the other team
+//	does not accept than the event will be flaged for review by
+// an admin.
