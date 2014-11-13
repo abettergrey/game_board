044e1f484cb318031e78743ca597c643a3012be9
Brett Yeager <abettergrey@gmail.com>
2014-11-13 09:53:39 -0500

    Added a create table

--- index.php
+++ index.php
@@ -21,11 +21,45 @@ $dbname="game_board";
 $mysqli = new mysqli($hostname, $username, $password, $dbname);
 checkConnect($mysqli);
 echo 'The database is connected';
+
+//This will only fire if the database is connected
+if($mysqli)
+{
+	createTables($mysqli);
+}
 function checkConnect($mysqli)
 {
 	if($mysqli->connect_errno) 
 	{
 		die('You done fucked up, and here is why ' . $mysqli->connect_error);
 		exit();
+	}
+}
+
+function createTables($mysqli)
+{
+	if($result = $mysqli->query("SELECT id FROM events LIMIT 1"))
+	{
+		$row = $result->fetch_object();
+		$id = $row->id;
+		$result->close();
+	}
+	if(!$id)
+	{
+		$sql = "CREATE TABLE events 
+				(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY( id ),
+				team_one_id INT,
+				team_two_id INT,
+				team_one_score INT,
+				team_two_socre INT, 
+				game_name VARCHAR(30),
+				game_type VARCHAR(30),
+				winning_team INT,
+				closed BOOLEAN,
+				flag BOOLEAN,
+				FOREIGN KEY (team_one_id) REFERENCES teams (id),
+				FOREIGN KEY (team_two_id) REFERENCES teams (id),
+				FOREIGN KEY (winning_team) REFERENCES teams (id))";
+				
 	}
 }
