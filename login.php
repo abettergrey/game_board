005878941bf0c2049177648b2171b63a5fae8cc7
Brett Yeager <abettergrey@gmail.com>
2014-11-26 17:14:57 -0500

    redirct to login page

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ function show_login()
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"</script>
 	</script></head><body>';
 
 	echo '<div class="bs-example">
