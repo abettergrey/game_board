fae714d352e2014bcd4c728c0b8d839d0d3407ee
Brett Yeager <abettergrey@gmail.com>
2014-12-06 13:37:33 -0500

    Building stuff

--- index.php
+++ index.php
@@ -412,7 +412,7 @@ function populateTable($mysqli)
 			}
 			else
 			{
-				$output .= '<td>' . $user_team . $row[1] .'</td>';
+				$output .= '<td>' . $user_team . $is_my_team1 .' fdsa</td>';
 			}
 			echo $output;
 		}
