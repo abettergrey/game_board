5470805aa16ab699390ba537dcb38b8fda7047e8
Brett Yeager <abettergrey@gmail.com>
2014-12-06 13:04:27 -0500

    Building stuff

--- index.php
+++ index.php
@@ -408,10 +408,10 @@ function populateTable($mysqli)
 }
 
 function convert_int_to_name($num)
-{	echo $num;
+{	
 	global $mysqli;
 	if($result = $mysqli->query("SELECT team_name FROM team WHERE id = $num"))
-	{
+	{	echo "yes";
 		$row = $result->fetch_object();
 
 		return $row;
